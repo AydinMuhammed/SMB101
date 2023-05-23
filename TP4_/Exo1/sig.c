07953335e3d5804af285c7093e3370a8602c711b
Muhammed <aydin.muhammedemin67@gmail.com>
2023-05-23 11:42:34 +0200

    TP4

--- TP4_/Exo1/sig.c
+++ TP4_/Exo1/sig.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <signal.h>
 
 int counter = 0;
@@ -30,12 +31,12 @@ int main()
     return 0;
 }
 
-// pour demarrer le programme : ./sig
+/* pour demarrer le programme : ./sig
 
-// pour trouver le PID du programme : ps aux | grep sig (remplacer PID par le résulat obtenu)
+pour trouver le PID du programme : ps aux | grep sig (remplacer PID par le résulat obtenu)
 
-// pour envoyer un signal SIGINT : - faire CTRL + C dans le terminal (vous pourrez observer en temps réel la sortie du programme)
-//                                 - ou faire kill -SIGINT <PID> (mais vous ne pourrez pas observer en temps réel la sortie du programme)
+pour envoyer un signal SIGINT : - faire CTRL + C dans le terminal (vous pourrez observer en temps réel la sortie du programme)
+                                 - ou faire kill -SIGINT <PID> (mais vous ne pourrez pas observer en temps réel la sortie du programme)
 
-// pour envoyer un signal SIGTERM : kill -SIGTERM <PID>
+pour envoyer un signal SIGTERM : kill -SIGTERM <PID> */
 
