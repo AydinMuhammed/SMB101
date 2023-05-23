2e429a962fd0dfb87b810cf0b5ed2b3ec956c866
Muhammed <aydin.muhammedemin67@gmail.com>
2023-05-23 14:05:47 +0200

    exo 2 tp4

--- TP4_/Exo2/ping.c
+++ TP4_/Exo2/ping.c
@@ -8,39 +8,42 @@
 int score_pere = 0, score_enfant = 0;
 
 void handle_sigusr1(int sig) {
-    printf("signal reçu\n");
-    sleep(1);
     int random_number = rand() % 2;
     if (random_number == 1) {
         printf("Le papa renvoie la balle\n");
         kill(getppid(), SIGUSR1);
+        score_pere++;
     } else {
         printf("Le papa manque la balle\n");
         kill(getppid(), SIGUSR2);
+        score_enfant++;
     }
+    printf("Signal reçu (score : papa = %d, enfant = %d)\n", score_pere, score_enfant);
+    sleep(1);
 }
 
 void handle_sigusr2(int sig) {
-    printf("signal reçu\n");
-    sleep(1);
     int random_number = rand() % 2;
     if (random_number == 1) {
         printf("Le enfant renvoie la balle\n");
         kill(getppid(), SIGUSR2);
+        score_enfant++;
     } else {
         printf("Le enfant manque la balle\n");
         kill(getppid(), SIGUSR1);
+        score_pere++;
     }
+    printf("Signal reçu (score : papa = %d, enfant = %d)\n", score_pere, score_enfant);
+    sleep(1);
 }
 
 void handle_sigterm(int sig) {
     if (score_pere == 13) {
         printf("J'ai gagné !\n");
-        kill(getpid(), SIGTERM);
     } else if (score_enfant == 13) {
         printf("Mon enfant a gagné !\n");
-        kill(getpid(), SIGTERM);
     }
+    exit(EXIT_SUCCESS);
 }
 
 int main() {
@@ -56,16 +59,34 @@ int main() {
         exit(EXIT_FAILURE);
     } else if (pid == 0) {
         // processus enfant
-        while (1) {
+        while (score_enfant < 13 && score_pere < 13) {
             sleep(1);
         }
     } else {
         // processus père
         kill(pid, SIGUSR1);
-        while (1) {
+        while (score_enfant < 13 && score_pere < 13) {
             sleep(1);
         }
     }
+
+    // Nettoyage et terminaison
+    if (pid == 0) {
+        // processus enfant
+        if (score_enfant == 13) {
+            printf("Mon enfant a gagné !\n");
+        }
+    } else {
+        // processus père
+        if (score_pere == 13) {
+            printf("J'ai gagné !\n");
+        }
+    }
+
+    printf("Appuyez sur une touche pour quitter...\n");
+    getchar();
+    
     return 0;
 }
 
+// pour tester le programme : ./ping
